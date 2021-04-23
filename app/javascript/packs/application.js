c3db35e1e358dd09b242394688b24b09c5a1cfe5
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-04-23 19:39:28 +0700

    add bootstrap fontawesome devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+// Moi them vao
+import '../stylesheets/application'
+
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -7,6 +11,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
 
 Rails.start()
 Turbolinks.start()
