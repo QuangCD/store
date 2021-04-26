491cccc3c030584cd42cb65b1098d12b2875c17f
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-04-26 20:15:12 +0700

    add transplane

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
     before_action :set_locale
     private
     def set_locale
-        I18n.locale = params[:locale].to_sym || :en  
+        cookies[:locale] ||= :en
+        I18n.locale = cookies[:locale].to_sym || :en  
     end
 
 end
